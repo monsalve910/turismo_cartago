9d7a4b98d53fe238d646eb117daedf70a49eaedc
monsalve910 <monsalve8000@gmail.com>
2026-04-13 11:17:34 -0500

    primer commit

---
+++ database/migrations/2026_04_13_160708_create_lugares_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lugares', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('ruta_id')->constrained()->onDelete('cascade');
+            $table->string('nombre');
+            $table->text('descripcion');
+            $table->integer('orden');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lugares');
+    }
+};
